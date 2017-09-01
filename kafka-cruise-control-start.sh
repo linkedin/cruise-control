94682bec44fd2d85345d5b9119c65124745948a1
Vlad Gorodetsky <bai@users.noreply.github.com>
2017-09-01 09:49:27 +0300

    Remove invalid UTF-8 byte sequence that breaks build on some platforms (#12)

--- kafka-cruise-control-start.sh
+++ kafka-cruise-control-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2017 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License"). 
+# Copyright 2017 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License").
 # See License in the project root for license information.
 
 if [ $# -lt 1 ];
