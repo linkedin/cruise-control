b3bce3c538fad3310fcc895e5dde262660b15ee6
Jiangjie Qin <jqin@jqin-ld2.linkedin.biz>
2018-04-05 11:08:01 -0700

    Remove unused import

--- cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/analyzer/PreferredLeaderElectionGoalTest.java
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/analyzer/PreferredLeaderElectionGoalTest.java
@@ -24,7 +24,6 @@ import org.apache.kafka.common.TopicPartition;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 
 public class PreferredLeaderElectionGoalTest {
