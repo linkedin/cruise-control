8af898497878b52076dcadd4724efc2412b379d5
Jiangjie Qin <jqin@jqin-ld2.linkedin.biz>
2018-04-07 14:50:55 -0700

    Rebase on master and fix some tests

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/MetricSamplerPartitionAssignor.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/MetricSamplerPartitionAssignor.java
@@ -6,6 +6,7 @@ package com.linkedin.kafka.cruisecontrol.monitor.sampling;
 
 import com.linkedin.cruisecontrol.common.CruiseControlConfigurable;
 import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.Configurable;
 import org.apache.kafka.common.TopicPartition;
 
 import java.util.List;
@@ -13,8 +14,11 @@ import java.util.Set;
 
 /**
  * The interface to assign the partitions to the metric samplers.
+ *
+ * The class has to extend both {@link CruiseControlConfigurable} and {@link Configurable} to ensure it is configured
+ * properly.
  */
-public interface MetricSamplerPartitionAssignor extends CruiseControlConfigurable {
+public interface MetricSamplerPartitionAssignor extends CruiseControlConfigurable, Configurable {
 
   /**
    * Assign the partitions in the cluster to the metric fetchers.
