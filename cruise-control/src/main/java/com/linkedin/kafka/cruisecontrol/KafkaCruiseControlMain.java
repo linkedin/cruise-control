2f69800493b57319589b95ee677c2f50d888d6ea
Naresh <vnareshkumar@gmail.com>
2018-10-11 12:47:18 +0530

    Allow bind address to be specified (#275)

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/KafkaCruiseControlMain.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/KafkaCruiseControlMain.java
@@ -16,6 +16,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlet.DefaultServlet;
+import java.net.InetSocketAddress;
 
 /**
  * The main class to run Kafka Cruise Control.
@@ -47,6 +48,14 @@ public class KafkaCruiseControlMain {
         printErrorMessageAndDie();
       }
     }
+    // Future ssl bits
+    String protocol = "http";
+
+    // For security reasons listen on the loopback address by default
+    String hostname = "127.0.0.1";
+    if (args.length > 2) {
+      hostname = args[2];
+    }
 
     MetricRegistry dropwizardMetricsRegistry = new MetricRegistry();
     JmxReporter jmxReporter = JmxReporter.forRegistry(dropwizardMetricsRegistry).inDomain(METRIC_DOMAIN).build();
@@ -55,7 +64,9 @@ public class KafkaCruiseControlMain {
     AsyncKafkaCruiseControl kafkaCruiseControl = new AsyncKafkaCruiseControl(new KafkaCruiseControlConfig(props),
                                                                              dropwizardMetricsRegistry);
 
-    Server server = new Server(port);
+    // Listen on a specific host & port combination
+    Server server = new Server(new InetSocketAddress(hostname, port));
+    // Define context for servlet
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
     server.setHandler(context);
@@ -80,11 +91,11 @@ public class KafkaCruiseControlMain {
     kafkaCruiseControl.startUp();
     server.start();
     System.out.println("Application directory: " + System.getProperty("user.dir"));
-    System.out.println("Kafka Cruise Control started.");
+    System.out.println("Kafka Cruise Control started on " + protocol + "://" + hostname + ":" + port);
   }
 
   private static void printErrorMessageAndDie() {
-    System.out.println(String.format("USAGE: java %s cruisecontrol.properties [port]", KafkaCruiseControlMain.class.getSimpleName()));
+    System.out.println(String.format("USAGE: java %s cruisecontrol.properties [port] [ipaddress|hostname]", KafkaCruiseControlMain.class.getSimpleName()));
     System.exit(-1);
   }
 }
