1a462e5807b778bfc566d8df67fbca486a1456d4
kun du <kdu@linkedin.com>
2018-10-15 16:15:49 -0700

    Improve sample loading efficiency in Cruise Control booting up process. (#359)

--- cruise-control-core/src/main/java/com/linkedin/cruisecontrol/monitor/sampling/aggregator/RawMetricValues.java
+++ cruise-control-core/src/main/java/com/linkedin/cruisecontrol/monitor/sampling/aggregator/RawMetricValues.java
@@ -185,21 +185,25 @@ public class RawMetricValues extends WindowIndexedArrays {
    *
    * @param startingWindowIndex the starting index of the windows to reset.
    * @param numWindowIndexesToReset the number of windows to reset.
+   * @return number of samples abandoned in window clearing process. The abandoned samples are samples in the windows which get reset.
    */
-  public synchronized void resetWindowIndexes(long startingWindowIndex, int numWindowIndexesToReset) {
+  public synchronized int resetWindowIndexes(long startingWindowIndex, int numWindowIndexesToReset) {
     if (inValidRange(startingWindowIndex)
         || inValidRange(startingWindowIndex + numWindowIndexesToReset - 1)) {
       throw new IllegalStateException("Should never reset a window index that is in the valid range");
     }
-    LOG.debug("Resetting window index [{}, {}]", startingWindowIndex,
-              startingWindowIndex + numWindowIndexesToReset - 1);
     // We are not resetting all the data here. The data will be interpreted to 0 if count is 0.
+    int numAbandonedSamples = 0;
     for (long i = startingWindowIndex; i < startingWindowIndex + numWindowIndexesToReset; i++) {
       int index = arrayIndex(i);
+      numAbandonedSamples += _counts[index];
       _counts[index] = 0;
       _validity.clear(index);
       _extrapolations.clear(index);
     }
+    LOG.trace("Resetting window index [{}, {}], abandon {} samples.", startingWindowIndex,
+              startingWindowIndex + numWindowIndexesToReset - 1, numAbandonedSamples);
+    return numAbandonedSamples;
   }
 
   /**
