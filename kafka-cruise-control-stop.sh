aebb391babb4b7b33f74da794b283fd8dbbfbb19
Soyeon (Emily) Ki <emilyki@outlook.com>
2019-06-01 20:52:49 -0400

    Add kafka-cruise-control-stop.sh. (#579) (#726)

---
+++ kafka-cruise-control-stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright 2019 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License").
+# See License in the project root for license information.
+
+SIGNAL=${SIGNAL:-TERM}
+PIDS=$(ps ax | grep -i 'cruise-control' | grep java | grep -v grep | awk '{print $1}')
+
+if [ -z "$PIDS" ]; then
+  echo "No cruise-control to stop"
+  exit 1
+else
+  kill -s $SIGNAL $PIDS
+fi
