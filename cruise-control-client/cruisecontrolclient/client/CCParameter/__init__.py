1e51ab9a5b0413d0dbeb775d9408d908fc4afbda
Michael Gruben Trejo <mgrubent@linkedin.com>
2019-06-18 14:59:19 -0700

    Also remove linkedin prefix from __init__

--- cruise-control-client/cruisecontrolclient/client/CCParameter/__init__.py
+++ cruise-control-client/cruisecontrolclient/client/CCParameter/__init__.py
@@ -1,9 +1,9 @@
 # Flatten the imports so that packages above just need:
 # from CCParameter import {SomeConcreteParameterClass}
-from linkedin.cruisecontrolclient.client.CCParameter.BooleanParameter import *  # noqa
-from linkedin.cruisecontrolclient.client.CCParameter.CommaSeparatedParameter import *  # noqa
-from linkedin.cruisecontrolclient.client.CCParameter.Parameter import *  # noqa
-from linkedin.cruisecontrolclient.client.CCParameter.RegularExpressionParameter import *  # noqa
-from linkedin.cruisecontrolclient.client.CCParameter.SetOfChoicesParameter import *  # noqa
-from linkedin.cruisecontrolclient.client.CCParameter.TimeStampParameter import *  # noqa
-from linkedin.cruisecontrolclient.client.CCParameter.PositiveIntegerParameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.BooleanParameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.CommaSeparatedParameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.Parameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.RegularExpressionParameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.SetOfChoicesParameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.TimeStampParameter import *  # noqa
+from cruisecontrolclient.client.CCParameter.PositiveIntegerParameter import *  # noqa
