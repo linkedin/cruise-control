5a6cf3ec513894853dba489a550b63fb5b93b57e
Adem Efe Gencer <agencer@linkedin.com>
2019-07-09 13:23:24 -0700

    Fix NPE upon retrieving the response of anomaly detector. (#820)

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/notifier/NoopNotifier.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/notifier/NoopNotifier.java
@@ -59,7 +59,12 @@ public class NoopNotifier implements AnomalyNotifier {
 
   @Override
   public Map<AnomalyType, Float> selfHealingEnabledRatio() {
-    return null;
+    Map<AnomalyType, Float> selfHealingEnabledRatio = new HashMap<>(AnomalyType.cachedValues().size());
+    for (AnomalyType anomalyType : AnomalyType.cachedValues()) {
+      selfHealingEnabledRatio.put(anomalyType, 0.0f);
+    }
+
+    return selfHealingEnabledRatio;
   }
 
   @Override
