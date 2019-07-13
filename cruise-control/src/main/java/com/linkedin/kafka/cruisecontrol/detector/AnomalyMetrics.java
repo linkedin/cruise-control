fde604b8c134f35995fa86eae870de78e081ac9b
Adem Efe Gencer <agencer@linkedin.com>
2019-07-12 17:25:30 -0700

    Address the feedback.

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/AnomalyMetrics.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/AnomalyMetrics.java
@@ -19,10 +19,10 @@ public class AnomalyMetrics {
    *   <li>Mean time between anomalies: The mean time between detected anomalies, while there is no ongoing execution.</li>
    *   <li>Mean time to start fix: Mean time between the detection of earliest ongoing anomaly and start of a proposal
    *   execution. This metric covers executions started both by anomaly detector and as a result of on-demand requests.
-   *   This metric excludes the time of the current ongoing unfixed/ </li>
-   *   <li>Number of self healing started: Successful self-healing fixes started by anomaly detector</li>
+   *   This metric excludes the time of the current ongoing unfixed anomaly.</li>
+   *   <li>Number of self healing started: Successful self-healing fixes started by anomaly detector.</li>
    *   <li>Ongoing anomaly duration: The time (in milliseconds) from the start of the earliest unfixed (ongoing) anomaly
-   *   to the current time for which no fix has been started. 0, if there is no ongoing anomaly.</li>
+   *   to the current time for which no fix has been started. 0, if there is no unfixed (ongoing) anomaly.</li>
    * </ol>
    *
    * @param meanTimeBetweenAnomalies Mean time between anomalies by the corresponding type.
