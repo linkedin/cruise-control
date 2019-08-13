c7060f760d52510831e7af102a87427f16ffa765
Adem Efe Gencer <agencer@linkedin.com>
2019-08-12 17:14:12 -0700

    Fix populating replica placement info regardless of the populateReplicaPlacementInfo parameter. (#900)

--- cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/analyzer/RandomClusterTest.java
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/analyzer/RandomClusterTest.java
@@ -203,7 +203,7 @@ public class RandomClusterTest {
       }
 
       BrokerCapacityInfo brokerCapacityInfo = new BrokerCapacityInfo(brokerCapacity);
-      clusterWithNewBroker.createBroker(b.rack().id(), Integer.toString(b.id()), b.id(), brokerCapacityInfo);
+      clusterWithNewBroker.createBroker(b.rack().id(), Integer.toString(b.id()), b.id(), brokerCapacityInfo, false);
     }
 
     for (Map.Entry<String, List<Partition>> entry : clusterModel.getPartitionsByTopic().entrySet()) {
@@ -229,7 +229,8 @@ public class RandomClusterTest {
       clusterWithNewBroker.createBroker(Integer.toString(i),
                                         Integer.toString(i + clusterModel.brokers().size() - 1),
                                         i + clusterModel.brokers().size() - 1,
-                                        commonBrokerCapacityInfo);
+                                        commonBrokerCapacityInfo,
+                                        false);
       clusterWithNewBroker.setBrokerState(i + clusterModel.brokers().size() - 1, Broker.State.NEW);
     }
 
