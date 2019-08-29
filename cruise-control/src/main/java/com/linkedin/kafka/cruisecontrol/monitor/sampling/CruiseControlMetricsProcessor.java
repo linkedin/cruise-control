646baf4c0cdd9efa69d77ba710fe996a000cb963
kun du <kdu@linkedin.com>
2019-08-29 16:33:52 -0700

    Fix bug in processing metrics records for brokers not listed in metadata (#923)

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/CruiseControlMetricsProcessor.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/CruiseControlMetricsProcessor.java
@@ -22,10 +22,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.linkedin.kafka.cruisecontrol.monitor.MonitorUtils.getRackHandleNull;
+import static com.linkedin.kafka.cruisecontrol.monitor.sampling.SamplingUtils.UNRECOGNIZED_BROKER_ID;
 import static com.linkedin.kafka.cruisecontrol.monitor.sampling.SamplingUtils.buildBrokerMetricSample;
 import static com.linkedin.kafka.cruisecontrol.monitor.sampling.SamplingUtils.buildPartitionMetricSample;
 import static com.linkedin.kafka.cruisecontrol.monitor.sampling.SamplingUtils.leaderDistribution;
-
 
 /**
  * Process the raw metrics collected by {@link CruiseControlMetricsReporterSampler} from the Kafka cluster.
@@ -72,6 +72,10 @@ public class CruiseControlMetricsProcessor {
       // Compute cached number of cores by broker id if they have not been cached already.
       _cachedNumCoresByBroker.computeIfAbsent(brokerId, bid -> {
         Node node = cluster.nodeById(bid);
+        if (node == null) {
+          LOG.warn("Received metrics from unrecognized broker {}.", bid);
+          return null;
+        }
         BrokerCapacityInfo capacity = _brokerCapacityConfigResolver.capacityForBroker(getRackHandleNull(node), node.host(), bid);
         // No mapping shall be recorded if capacity is estimated, but estimation is not allowed.
         return (!_allowCpuCapacityEstimation && capacity.isEstimated()) ? null : capacity.numCpuCores();
@@ -106,10 +110,10 @@ public class CruiseControlMetricsProcessor {
     _brokerLoad.forEach((broker, load) -> load.prepareBrokerMetrics(cluster, broker, _maxMetricTimestamp));
 
     // Get partition metric samples.
-    int skippedPartition = 0;
+    Map<Integer, Integer> skippedPartitionByBroker = null;
     Set<PartitionMetricSample> partitionMetricSamples = new HashSet<>();
     if (samplingMode == MetricSampler.SamplingMode.ALL || samplingMode == MetricSampler.SamplingMode.PARTITION_METRICS_ONLY) {
-      skippedPartition = addPartitionMetricSamples(cluster, partitionsDotNotHandled, partitionMetricSamples);
+      skippedPartitionByBroker = addPartitionMetricSamples(cluster, partitionsDotNotHandled, partitionMetricSamples);
     }
 
     // Get broker metric samples.
@@ -120,9 +124,9 @@ public class CruiseControlMetricsProcessor {
     }
 
     LOG.info("Generated {}{} partition metric samples and {}{} broker metric samples for timestamp {}.",
-             partitionMetricSamples.size(), skippedPartition > 0 ? "(" + skippedPartition + " skipped)" : "",
-             brokerMetricSamples.size(), skippedBroker > 0 ? "(" + skippedBroker + " skipped)" : "",
-             _maxMetricTimestamp);
+             partitionMetricSamples.size(), (skippedPartitionByBroker != null && !skippedPartitionByBroker.isEmpty()) ?
+             String.format("(%s skipped by broker %s)", skippedPartitionByBroker.values().stream().mapToInt(v -> v).sum(), skippedPartitionByBroker) : "",
+             brokerMetricSamples.size(), skippedBroker > 0 ? "(" + skippedBroker + " skipped)" : "", _maxMetricTimestamp);
     return new MetricSampler.Samples(partitionMetricSamples, brokerMetricSamples);
   }
 
@@ -137,29 +141,28 @@ public class CruiseControlMetricsProcessor {
    * @param cluster Kafka cluster
    * @param partitionsDotNotHandled The partitions to get samples. The topic partition name may have dots.
    * @param partitionMetricSamples The set to add the partition samples to.
-   * @return The number of skipped partitions.
+   * @return The number of skipped partitions by broker ids. A broker id of {@link SamplingUtils#UNRECOGNIZED_BROKER_ID}
+   *         indicates unrecognized broker.
    */
-  private int addPartitionMetricSamples(Cluster cluster,
-                                        Set<TopicPartition> partitionsDotNotHandled,
-                                        Set<PartitionMetricSample> partitionMetricSamples) {
-    int skippedPartition = 0;
+  private Map<Integer, Integer> addPartitionMetricSamples(Cluster cluster,
+                                                          Set<TopicPartition> partitionsDotNotHandled,
+                                                          Set<PartitionMetricSample> partitionMetricSamples) {
+    Map<Integer, Integer> skippedPartitionByBroker = new HashMap<>();
     Map<Integer, Map<String, Integer>> leaderDistribution = leaderDistribution(cluster);
     for (TopicPartition tpDotNotHandled : partitionsDotNotHandled) {
       try {
-        PartitionMetricSample sample = buildPartitionMetricSample(cluster, leaderDistribution, tpDotNotHandled,
-                                                                  _brokerLoad, _maxMetricTimestamp, _cachedNumCoresByBroker);
+        PartitionMetricSample sample = buildPartitionMetricSample(cluster, leaderDistribution, tpDotNotHandled, _brokerLoad,
+                                                                  _maxMetricTimestamp, _cachedNumCoresByBroker, skippedPartitionByBroker);
         if (sample != null) {
           LOG.trace("Added partition metrics sample for {}.", tpDotNotHandled);
           partitionMetricSamples.add(sample);
-        } else {
-          skippedPartition++;
         }
       } catch (Exception e) {
         LOG.error("Error building partition metric sample for {}.", tpDotNotHandled, e);
-        skippedPartition++;
+        skippedPartitionByBroker.merge(UNRECOGNIZED_BROKER_ID, 1, Integer::sum);
       }
     }
-    return skippedPartition;
+    return skippedPartitionByBroker;
   }
 
   /**
