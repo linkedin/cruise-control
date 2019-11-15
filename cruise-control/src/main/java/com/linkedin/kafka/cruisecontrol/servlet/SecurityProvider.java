5d87574c518ca61b4bdb4b0b2e49508b40471894
Viktor Somogyi-Vass <viktorsomogyi@gmail.com>
2019-11-15 14:01:39 +0100

    Javadoc for SecurityProvider

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/servlet/SecurityProvider.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/servlet/SecurityProvider.java
@@ -11,13 +11,33 @@ import org.eclipse.jetty.security.LoginService;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * This class delegates to the Jetty implementation of authentication features and is used for setting
+ * up Cruise Control security to provide authentication and authorization rules over the WEB API.
+ */
 public interface SecurityProvider {
 
+  /**
+   * Creates or gets a list of constraints that are put on the API endpoints, for instance only
+   * allow certain roles on certain endpoints.
+   */
   List<ConstraintMapping> constraintMappings();
 
+  /**
+   * Associates a username, credentials and roles with a {@link org.eclipse.jetty.server.UserIdentity}
+   * that will be used by Jetty to manage the authentication.
+   */
   LoginService loginService();
 
+  /**
+   * Defines the request authentication method which is responsible to send challenges
+   * according to authentication method and decide if the user has valid credentials according
+   * to the authentication method.
+   */
   Authenticator authenticator();
 
+  /**
+   * A list of strings that define the possible roles in Cruise Control.
+   */
   Set<String> roles();
 }
