dad000f203b71e36c52a4eb8fba3a86e38693bda
Jeffrey Fei <jeffreyfei@msn.com>
2019-12-18 10:57:11 -0800

    Add build api wiki (#42)
    * Add gradle task to build api wiki
    
    * Add dependency for building api wiki

---
+++ build_api_wiki.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR=$(pwd)
+SCHEMA_WORKDIR="$BASE_DIR/cruise-control/src/yaml"
+WIKI_TARGET="$BASE_DIR/target/api_wiki"
+
+rm -rf $WIKI_TARGET
+mkdir -p $WIKI_TARGET
+
+cd $SCHEMA_WORKDIR; npx redoc-cli bundle base.yaml
+mv -v $SCHEMA_WORKDIR/redoc-static.html $WIKI_TARGET
