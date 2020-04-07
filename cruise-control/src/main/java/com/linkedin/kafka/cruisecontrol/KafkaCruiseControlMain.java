e9e62ac0368d576d93403d23b1f74cbfbcd8b61b
Costi Ciudatu <ciudatu@adobe.com>
2020-04-07 20:57:51 +0300

    #1172: The main class hides any exceptions and prints a usage message

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/KafkaCruiseControlMain.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/KafkaCruiseControlMain.java
@@ -25,18 +25,16 @@ public class KafkaCruiseControlMain {
    */
   public static void main(String[] args) throws Exception {
     if (args.length == 0) {
-      printErrorMessageAndDie();
+      throw new IllegalArgumentException(
+              String.format("USAGE: java %s cruisecontrol.properties [port] [ipaddress|hostname]",
+                      KafkaCruiseControlMain.class.getSimpleName()));
     }
-    try {
-      KafkaCruiseControlConfig config = readConfig(args[0]);
-      Integer port = parsePort(args, config);
-      String hostname = parseHostname(args, config);
-      KafkaCruiseControlApp app = new KafkaCruiseControlApp(config, port, hostname);
-      app.registerShutdownHook();
-      app.start();
-    } catch (IOException | IllegalArgumentException e) {
-      printErrorMessageAndDie();
-    }
+    KafkaCruiseControlConfig config = readConfig(args[0]);
+    Integer port = parsePort(args, config);
+    String hostname = parseHostname(args, config);
+    KafkaCruiseControlApp app = new KafkaCruiseControlApp(config, port, hostname);
+    app.registerShutdownHook();
+    app.start();
   }
 
   private static KafkaCruiseControlConfig readConfig(String propertiesFile) throws IOException {
@@ -44,41 +42,23 @@ public class KafkaCruiseControlMain {
     try (InputStream propStream = new FileInputStream(propertiesFile)) {
       props.load(propStream);
     }
-
     return new KafkaCruiseControlConfig(props);
   }
 
   private static Integer parsePort(String[] args, KafkaCruiseControlConfig config) {
-    Integer port = null;
-    try {
-      if (args.length > 1) {
-        port = Integer.parseInt(args[1]);
-      } else {
-        return config.getInt(WebServerConfig.WEBSERVER_HTTP_PORT_CONFIG);
-      }
-    } catch (Exception e) {
-      printErrorMessageAndDie();
+    if (args.length > 1) {
+      return Integer.parseInt(args[1]);
+    } else {
+      return config.getInt(WebServerConfig.WEBSERVER_HTTP_PORT_CONFIG);
     }
-    return port;
   }
 
   private static String parseHostname(String[] args, KafkaCruiseControlConfig config) {
-    String hostname = null;
-    try {
-      if (args.length > 2) {
-        hostname = args[2];
-      } else {
-        return config.getString(WebServerConfig.WEBSERVER_HTTP_ADDRESS_CONFIG);
-      }
-    } catch (Exception e) {
-      printErrorMessageAndDie();
+    if (args.length > 2) {
+      return args[2];
+    } else {
+      return config.getString(WebServerConfig.WEBSERVER_HTTP_ADDRESS_CONFIG);
     }
-    return hostname;
   }
 
-  private static void printErrorMessageAndDie() {
-    System.out.println(String.format("USAGE: java %s cruisecontrol.properties [port] [ipaddress|hostname]",
-                                     KafkaCruiseControlMain.class.getSimpleName()));
-    System.exit(-1);
-  }
 }
