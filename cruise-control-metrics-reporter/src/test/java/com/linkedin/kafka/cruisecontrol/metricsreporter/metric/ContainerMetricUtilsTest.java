c1d4274c8eec99443539595bc22808f6d5720910
Kyle Liberti <kliberti@redhat.com>
2020-07-27 19:32:40 -0400

    Fix indentation to from 4 to 2 spaces for consistency with project
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- cruise-control-metrics-reporter/src/test/java/com/linkedin/kafka/cruisecontrol/metricsreporter/metric/ContainerMetricUtilsTest.java
+++ cruise-control-metrics-reporter/src/test/java/com/linkedin/kafka/cruisecontrol/metricsreporter/metric/ContainerMetricUtilsTest.java
@@ -15,41 +15,41 @@ import static org.junit.Assert.assertEquals;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ContainerMetricUtils.class)
 public class ContainerMetricUtilsTest {
-    
-    private static final double DELTA = 0.01;
 
-    private void mockGetContainerProcessCpuLoad(int processors, double cpuQuota, double cpuPeriod, double cpuUtil, double expectedLoad)
-            throws Exception {
-        PowerMock.mockStaticPartial(ContainerMetricUtils.class,
-                "getAvailableProcessors",
-                "getCpuPeriod",
-                "getCpuQuota");
+  private static final double DELTA = 0.01;
 
-        PowerMock.expectPrivate(ContainerMetricUtils.class, "getAvailableProcessors").andReturn(processors);
-        PowerMock.expectPrivate(ContainerMetricUtils.class, "getCpuPeriod").andReturn(cpuPeriod);
-        PowerMock.expectPrivate(ContainerMetricUtils.class, "getCpuQuota").andReturn(cpuQuota);
-        PowerMock.expectPrivate(ContainerMetricUtils.class, "getCpuQuota").andReturn(cpuQuota);
+  private void mockGetContainerProcessCpuLoad(int processors, double cpuQuota, double cpuPeriod, double cpuUtil, double expectedLoad)
+    throws Exception {
+    PowerMock.mockStaticPartial(ContainerMetricUtils.class,
+      "getAvailableProcessors",
+      "getCpuPeriod",
+      "getCpuQuota");
 
-        PowerMock.replay(ContainerMetricUtils.class);
+    PowerMock.expectPrivate(ContainerMetricUtils.class, "getAvailableProcessors").andReturn(processors);
+    PowerMock.expectPrivate(ContainerMetricUtils.class, "getCpuPeriod").andReturn(cpuPeriod);
+    PowerMock.expectPrivate(ContainerMetricUtils.class, "getCpuQuota").andReturn(cpuQuota);
+    PowerMock.expectPrivate(ContainerMetricUtils.class, "getCpuQuota").andReturn(cpuQuota);
 
-        assertEquals(expectedLoad, ContainerMetricUtils.getContainerProcessCpuLoad(cpuUtil), DELTA);
-    }
+    PowerMock.replay(ContainerMetricUtils.class);
 
-    @Test
-    public void testGetContainerProcessCpuLoad() throws Exception {
-        /*
-         *  expectedContainerProcessCpuLoad = (cpuUtil * processors) / (cpuQuota / cpuPeriod)
-         */
-        mockGetContainerProcessCpuLoad(1, 100000.0, 100000.0, 1.0, 1.0);
-        mockGetContainerProcessCpuLoad(1, 100000.0, 100000.0, 0.5, 0.5);
-        mockGetContainerProcessCpuLoad(1, 50000.0, 100000.0, 0.5, 1.0);
-        mockGetContainerProcessCpuLoad(1, 75000.0, 100000.0, 0.5, 0.66);
+    assertEquals(expectedLoad, ContainerMetricUtils.getContainerProcessCpuLoad(cpuUtil), DELTA);
+  }
 
-        mockGetContainerProcessCpuLoad(2, 100000.0, 100000.0, 0.5, 1.0);
-        mockGetContainerProcessCpuLoad(2, 200000.0, 100000.0, 1.0, 1.0);
-        mockGetContainerProcessCpuLoad(2, 25000.0, 100000.0, 0.125, 1.0);
-        mockGetContainerProcessCpuLoad(2, 2500.0, 100000.0, 0.0125, 1.0);
+  @Test
+  public void testGetContainerProcessCpuLoad() throws Exception {
+    /*
+     *  expectedContainerProcessCpuLoad = (cpuUtil * processors) / (cpuQuota / cpuPeriod)
+     */
+    mockGetContainerProcessCpuLoad(1, 100000.0, 100000.0, 1.0, 1.0);
+    mockGetContainerProcessCpuLoad(1, 100000.0, 100000.0, 0.5, 0.5);
+    mockGetContainerProcessCpuLoad(1, 50000.0, 100000.0, 0.5, 1.0);
+    mockGetContainerProcessCpuLoad(1, 75000.0, 100000.0, 0.5, 0.66);
 
-        mockGetContainerProcessCpuLoad(2, ContainerMetricUtils.NO_CPU_QUOTA, 100000.0, 0.125, 0.125);
-    }
+    mockGetContainerProcessCpuLoad(2, 100000.0, 100000.0, 0.5, 1.0);
+    mockGetContainerProcessCpuLoad(2, 200000.0, 100000.0, 1.0, 1.0);
+    mockGetContainerProcessCpuLoad(2, 25000.0, 100000.0, 0.125, 1.0);
+    mockGetContainerProcessCpuLoad(2, 2500.0, 100000.0, 0.0125, 1.0);
+
+    mockGetContainerProcessCpuLoad(2, ContainerMetricUtils.NO_CPU_QUOTA, 100000.0, 0.125, 0.125);
+  }
 }
