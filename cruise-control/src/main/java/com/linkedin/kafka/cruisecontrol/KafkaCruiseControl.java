a7ff697af10f20c881239dfe41283460965edbaf
Adem Efe Gencer <agencer@linkedin.com>
2020-07-30 08:31:34 -0700

    Fix GOAL_VIOLATION detector getting stuck execution in GENERATING_PROPOSALS_FOR_EXECUTION state (#1291)

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/KafkaCruiseControl.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/KafkaCruiseControl.java
@@ -589,6 +589,8 @@ public class KafkaCruiseControl {
                                  concurrentInterBrokerPartitionMovements, concurrentIntraBrokerPartitionMovements,
                                  concurrentLeaderMovements, executionProgressCheckIntervalMs, replicaMovementStrategy,
                                  replicationThrottle, isTriggeredByUserRequest, uuid, isKafkaAssignerMode);
+    } else {
+      failGeneratingProposalsForExecution(uuid);
     }
   }
 
@@ -627,6 +629,8 @@ public class KafkaCruiseControl {
                                  removedBrokers, _loadMonitor, concurrentInterBrokerPartitionMovements, 0,
                                  concurrentLeaderMovements, executionProgressCheckIntervalMs, replicaMovementStrategy,
                                  replicationThrottle, isTriggeredByUserRequest, uuid, isKafkaAssignerMode);
+    } else {
+      failGeneratingProposalsForExecution(uuid);
     }
   }
 
@@ -667,6 +671,8 @@ public class KafkaCruiseControl {
       _executor.executeDemoteProposals(proposals, demotedBrokers, _loadMonitor, concurrentSwaps, concurrentLeaderMovements,
                                        executionProgressCheckIntervalMs, replicaMovementStrategy, replicationThrottle,
                                        isTriggeredByUserRequest, uuid);
+    } else {
+      failGeneratingProposalsForExecution(uuid);
     }
   }
 
