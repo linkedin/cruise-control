f578b311c08024d72c535298771e9e8310bfbe52
Lincong Li <lcli@linkedin.com>
2020-10-17 00:42:25 -0700

    Check whether a cluster is using JBOD when populate_disk_info is true (#1358)

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/servlet/handler/async/runnable/LoadRunnable.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/servlet/handler/async/runnable/LoadRunnable.java
@@ -7,14 +7,17 @@ package com.linkedin.kafka.cruisecontrol.servlet.handler.async.runnable;
 import com.linkedin.kafka.cruisecontrol.KafkaCruiseControl;
 import com.linkedin.kafka.cruisecontrol.async.progress.OperationProgress;
 import com.linkedin.kafka.cruisecontrol.exception.KafkaCruiseControlException;
+import com.linkedin.kafka.cruisecontrol.model.Broker;
 import com.linkedin.kafka.cruisecontrol.model.ClusterModel;
 import com.linkedin.kafka.cruisecontrol.monitor.ModelCompletenessRequirements;
+import com.linkedin.kafka.cruisecontrol.servlet.UserRequestException;
 import com.linkedin.kafka.cruisecontrol.servlet.parameters.ClusterLoadParameters;
 import com.linkedin.kafka.cruisecontrol.servlet.parameters.PartitionLoadParameters;
 import com.linkedin.kafka.cruisecontrol.servlet.response.stats.BrokerStats;
 
 import static com.linkedin.kafka.cruisecontrol.config.constants.MonitorConfig.MIN_VALID_PARTITION_RATIO_CONFIG;
 import static com.linkedin.kafka.cruisecontrol.servlet.parameters.ParameterUtils.DEFAULT_START_TIME_FOR_CLUSTER_MODEL;
+import static com.linkedin.kafka.cruisecontrol.servlet.parameters.ParameterUtils.POPULATE_DISK_INFO_PARAM;
 
 
 /**
@@ -63,12 +66,21 @@ public class LoadRunnable extends OperationRunnable {
       if (cachedBrokerStats != null) {
         return cachedBrokerStats;
       }
+    } else if (isClusterUsingJBOD()) {
+      throw new UserRequestException(String.format("Cannot set %s=true for non-JBOD Kafka clusters.", POPULATE_DISK_INFO_PARAM));
     }
+
     if (_start != DEFAULT_START_TIME_FOR_CLUSTER_MODEL) {
       return clusterModel(_modelCompletenessRequirements.minMonitoredPartitionsPercentage()).brokerStats(_kafkaCruiseControl.config());
     } else {
       return clusterModelFromEarliest().brokerStats(_kafkaCruiseControl.config());
     }
+  }
+
+  private boolean isClusterUsingJBOD() throws Exception {
+    ClusterModel clusterModel = _kafkaCruiseControl.loadMonitor().clusterCapacity();
+    // If and only if all brokers in the cluster are using JBOD, the cluster is considered to be using JBOD
+    return clusterModel.brokers().stream().allMatch(Broker::isUsingJBOD);
   }
 
   /**
