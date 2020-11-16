c75cc104686b5101cd3a14da85124274f82fb816
Lincong Li <lcli@linkedin.com>
2020-11-16 15:20:49 -0800

    Calculate balance lower bound for resource distribution lower bound with low utilization threshold

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/analyzer/goals/ResourceDistributionGoal.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/analyzer/goals/ResourceDistributionGoal.java
@@ -969,7 +969,8 @@ public abstract class ResourceDistributionGoal extends AbstractGoal {
    * @return The utilization lower threshold in percent for the {@link #resource()}
    */
   private double computeBalanceLowerThreshold(double clusterUtilization, OptimizationOptions optimizationOptions) {
-    return clusterUtilization * Math.max(0, (1 - balancePercentageWithMargin(optimizationOptions)));
+    return Math.max(clusterUtilization * Math.max(0, (1 - balancePercentageWithMargin(optimizationOptions))),
+                    _balancingConstraint.lowUtilizationThreshold(resource()));
   }
 
   /**
