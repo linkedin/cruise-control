6a226086785b981c60f132a2098bd52c6d246c8b
sasukerui <199181qr@sina.com>
2020-12-03 06:05:17 +0800

    Enable Kafka port retrieval from listeners config (#1246)
    Co-authored-by: 钱锐 <18120364@cnsuning.com>
    Co-authored-by: Adem Efe Gencer <agencer@agencer-mn1.linkedin.biz>

--- cruise-control-metrics-reporter/src/test/java/com/linkedin/kafka/cruisecontrol/metricsreporter/CruiseControlMetricsReporterTest.java
+++ cruise-control-metrics-reporter/src/test/java/com/linkedin/kafka/cruisecontrol/metricsreporter/CruiseControlMetricsReporterTest.java
@@ -11,6 +11,7 @@ import com.linkedin.kafka.cruisecontrol.metricsreporter.utils.CCKafkaClientsInte
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
@@ -38,15 +39,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporter.DEFAULT_BOOTSTRAP_SERVERS_HOST;
 import static com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporterConfig.CRUISE_CONTROL_METRICS_TOPIC_AUTO_CREATE_CONFIG;
 import static com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporterConfig.CRUISE_CONTROL_METRICS_TOPIC_NUM_PARTITIONS_CONFIG;
 import static com.linkedin.kafka.cruisecontrol.metricsreporter.CruiseControlMetricsReporterConfig.CRUISE_CONTROL_METRICS_TOPIC_REPLICATION_FACTOR_CONFIG;
 import static com.linkedin.kafka.cruisecontrol.metricsreporter.metric.RawMetricType.*;
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
+import java.util.regex.Pattern;
 
 public class CruiseControlMetricsReporterTest extends CCKafkaClientsIntegrationTestHarness {
   protected static final String TOPIC = "CruiseControlMetricsReporterTest";
+  private static final String HOST = "127.0.0.1";
 
   /**
    * Setup the unit test.
@@ -82,9 +86,8 @@ public class CruiseControlMetricsReporterTest extends CCKafkaClientsIntegrationT
     Properties props = new Properties();
     int port = CCKafkaTestUtils.findLocalPort();
     props.setProperty(CommonClientConfigs.METRIC_REPORTER_CLASSES_CONFIG, CruiseControlMetricsReporter.class.getName());
-    props.setProperty(KafkaConfig.ListenersProp(), "PLAINTEXT://127.0.0.1:" + port);
-    props.setProperty(CruiseControlMetricsReporterConfig.config(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG),
-                      "127.0.0.1:" + port);
+    props.setProperty(KafkaConfig.ListenersProp(), "PLAINTEXT://" + HOST + ":" + port);
+    props.setProperty(CruiseControlMetricsReporterConfig.config(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG), HOST + ":" + port);
     props.setProperty(CruiseControlMetricsReporterConfig.CRUISE_CONTROL_METRICS_REPORTER_INTERVAL_MS_CONFIG, "100");
     props.setProperty(CruiseControlMetricsReporterConfig.CRUISE_CONTROL_METRICS_TOPIC_CONFIG, TOPIC);
     props.setProperty(KafkaConfig.LogFlushIntervalMessagesProp(), "1");
@@ -201,5 +204,34 @@ public class CruiseControlMetricsReporterTest extends CCKafkaClientsIntegrationT
     // Check whether the topic config is updated
     topicDescription = adminClient.describeTopics(Collections.singleton(TOPIC)).values().get(TOPIC).get();
     assertEquals(2, topicDescription.partitions().size());
+  }
+
+  @Test
+  public void testGetKafkaBootstrapServersConfigure() {
+    // Test with a "listeners" config with a host
+    Map<Object, Object> brokerConfig = buildBrokerConfigs().get(0);
+    Map<String, Object> listenersMap = Collections.singletonMap(KafkaConfig.ListenersProp(), brokerConfig.get(KafkaConfig.ListenersProp()));
+    String bootstrapServers = CruiseControlMetricsReporter.getBootstrapServers(listenersMap);
+    String urlParse = "\\[?([0-9a-zA-Z\\-%._:]*)]?:(-?[0-9]+)";
+    Pattern urlParsePattern = Pattern.compile(urlParse);
+    assertTrue(urlParsePattern.matcher(bootstrapServers).matches());
+    assertEquals(HOST, bootstrapServers.split(":")[0]);
+
+    // Test with a "listeners" config without a host in the first listener.
+    String listeners = "SSL://:1234,PLAINTEXT://myhost:4321";
+    listenersMap = Collections.singletonMap(KafkaConfig.ListenersProp(), listeners);
+    bootstrapServers = CruiseControlMetricsReporter.getBootstrapServers(listenersMap);
+    assertTrue(urlParsePattern.matcher(bootstrapServers).matches());
+    assertEquals(DEFAULT_BOOTSTRAP_SERVERS_HOST, bootstrapServers.split(":")[0]);
+    assertEquals("1234", bootstrapServers.split(":")[1]);
+
+    // Test with "listeners" and "port" config together.
+    listenersMap = new HashMap<>(2);
+    listenersMap.put(KafkaConfig.ListenersProp(), listeners);
+    listenersMap.put(KafkaConfig.PortProp(), "43");
+    bootstrapServers = CruiseControlMetricsReporter.getBootstrapServers(listenersMap);
+    assertTrue(urlParsePattern.matcher(bootstrapServers).matches());
+    assertEquals(DEFAULT_BOOTSTRAP_SERVERS_HOST, bootstrapServers.split(":")[0]);
+    assertEquals("43", bootstrapServers.split(":")[1]);
   }
 }
