464e4695a5a551d71ac2a4f252354046b98fcefd
Lincong Li <lcli@linkedin.com>
2020-12-10 16:42:25 -0800

    Relax low resource utlization upper limit for resource distribution goals (#1420)

--- cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/analyzer/goals/GoalUtilsTest.java
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/analyzer/goals/GoalUtilsTest.java
@@ -13,23 +13,29 @@ import org.junit.Test;
 public class GoalUtilsTest {
 
   private static final double AVG_UTILIZATION_PERCENTAGE = 0.3;
-  private static final double RESOURCE_BALANCE_THRESHOLD = 1.3;
+  private static final double DEFAULT_RESOURCE_BALANCE_THRESHOLD = 1.3;
   private static final double GOAL_VIOLATION_DISTRIBUTION_THRESHOLD_MULTIPLIER = 1.2;
 
   @Test
   public void testComputeResourceUtilizationBalanceThreshold() {
     Resource resource = Resource.CPU;
 
-    // Verify 1: Low utilization and compute balance threshold lower bound
+    // Verify case 1: Low utilization and compute balance threshold lower bound
     verifyComputingResourceUtilizationBalanceThreshold(resource, true, 0.4, 0.0);
 
-    // Verify 2: Low utilization and compute balance threshold upper bound
-    verifyComputingResourceUtilizationBalanceThreshold(resource, false, 0.4, 0.4 * ResourceDistributionGoal.BALANCE_MARGIN);
+    // Verify case 2: Low utilization. The average utilization percentage * resource balance threshold > low resource utilization percentage
+    double expectedComputedBalanceUpperLimit = 0.3 * (1 + ((1.3 * 1.2) - 1) * ResourceDistributionGoal.BALANCE_MARGIN);
+    verifyComputingResourceUtilizationBalanceThreshold(resource, false, 0.4, expectedComputedBalanceUpperLimit);
 
-    // Verify 3: Not low utilization and compute balance threshold lower bound
+    // Verify case 3: Low utilization. The average utilization percentage * resource balance threshold < low resource utilization percentage
+    double lowUtilizationThreshold = 0.6;
+    expectedComputedBalanceUpperLimit = lowUtilizationThreshold * ResourceDistributionGoal.BALANCE_MARGIN;
+    verifyComputingResourceUtilizationBalanceThreshold(resource, false, lowUtilizationThreshold, expectedComputedBalanceUpperLimit);
+
+    // Verify case 4: Not low utilization and compute balance threshold lower bound
     verifyComputingResourceUtilizationBalanceThreshold(resource, true, 0.2, 0.3 * (1 - ((1.3 * 1.2) - 1) * ResourceDistributionGoal.BALANCE_MARGIN));
 
-    // Verify 4: Not low utilization and compute balance threshold upper bound
+    // Verify case 5: Not low utilization and compute balance threshold upper bound
     verifyComputingResourceUtilizationBalanceThreshold(resource, false, 0.2, 0.3 * (1 + ((1.3 * 1.2) - 1) * ResourceDistributionGoal.BALANCE_MARGIN));
   }
 
@@ -40,15 +46,17 @@ public class GoalUtilsTest {
 
     BalancingConstraint mockBalanceConstraint = EasyMock.mock(BalancingConstraint.class);
     EasyMock.expect(mockBalanceConstraint.lowUtilizationThreshold(resource)).andReturn(lowUtilizationThreshold).anyTimes();
-    EasyMock.expect(mockBalanceConstraint.resourceBalancePercentage(resource)).andReturn(RESOURCE_BALANCE_THRESHOLD).anyTimes();
+    EasyMock.expect(mockBalanceConstraint.resourceBalancePercentage(resource)).andReturn(DEFAULT_RESOURCE_BALANCE_THRESHOLD).anyTimes();
     EasyMock.expect(mockBalanceConstraint.goalViolationDistributionThresholdMultiplier()).
         andReturn(GOAL_VIOLATION_DISTRIBUTION_THRESHOLD_MULTIPLIER).anyTimes();
 
     EasyMock.replay(mockBalanceConstraint);
     double computedBalanceThreshold = GoalUtils.computeResourceUtilizationBalanceThreshold(AVG_UTILIZATION_PERCENTAGE,
-        resource,
-        mockBalanceConstraint, true, ResourceDistributionGoal.BALANCE_MARGIN,
-        isLowerThreshold);
+                                                                                           resource,
+                                                                                           mockBalanceConstraint,
+                                                                                           true,
+                                                                                           ResourceDistributionGoal.BALANCE_MARGIN,
+                                                                                           isLowerThreshold);
     EasyMock.verify(mockBalanceConstraint);
     Assert.assertEquals(expectedComputedBalanceThreshold, computedBalanceThreshold, 0.0);
   }
