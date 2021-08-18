9c32bff4131953d41c7f8a20d127cd3977e37c68
egyedt <81372484+egyedt@users.noreply.github.com>
2021-08-18 22:52:55 +0200

    Create ConfigProvider in Cruise-Control-Metrics-Reporter module and optimize dependencies (#1652)

---
+++ cruise-control-metrics-reporter/src/main/java/com/linkedin/kafka/cruisecontrol/metricsreporter/config/EnvConfigProvider.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License"). See License in the project root for license information.
+ */
+package com.linkedin.kafka.cruisecontrol.metricsreporter.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.kafka.common.config.ConfigData;
+import org.apache.kafka.common.config.ConfigException;
+import org.apache.kafka.common.config.provider.ConfigProvider;
+
+/**
+ * Enables config properties of the form ${env:ENV_KEY}
+ */
+public class EnvConfigProvider implements ConfigProvider {
+
+  private Map<String, String> _preConfiguredEnvironmentVariables;
+
+  @Override
+  public ConfigData get(String path) {
+    assertNoPath(path);
+    return new ConfigData(getEnv());
+  }
+
+  @Override
+  public ConfigData get(String path, Set<String> keys) {
+    assertNoPath(path);
+    Map<String, String> filtered = new HashMap<>(getEnv());
+    filtered.keySet().retainAll(keys);
+    return new ConfigData(filtered);
+  }
+
+  @Override
+  public void close() {
+    if (_preConfiguredEnvironmentVariables != null) {
+      _preConfiguredEnvironmentVariables.clear();
+    }
+  }
+
+  @Override
+  public void configure(Map<String, ?> configs) {
+    _preConfiguredEnvironmentVariables = configs.entrySet()
+      .stream()
+      .collect(Collectors.toMap(Entry::getKey, kv -> (String) kv.getValue()));
+  }
+
+  private static void assertNoPath(String path) {
+    if (path != null && !path.isEmpty()) {
+      throw new ConfigException("EnvConfigProvider does not support paths. Found: " + path);
+    }
+  }
+
+  private Map<String, String> getEnv() {
+    if (_preConfiguredEnvironmentVariables == null) {
+      return System.getenv();
+    } else {
+      return _preConfiguredEnvironmentVariables;
+    }
+  }
+
+}
