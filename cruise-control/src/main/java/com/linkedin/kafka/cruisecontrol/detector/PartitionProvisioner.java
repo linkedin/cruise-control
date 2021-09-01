019cb6bc27221435714301a50ccc4d0a0091574a
Adem Efe Gencer <agencer@linkedin.com>
2021-09-01 16:51:43 -0700

    Add a Partition Provisioner to support adding partitions to selected topics (#1678)

---
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/PartitionProvisioner.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2021 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License"). See License in the project root for license information.
+ */
+
+package com.linkedin.kafka.cruisecontrol.detector;
+
+import com.linkedin.kafka.cruisecontrol.KafkaCruiseControl;
+import com.linkedin.kafka.cruisecontrol.analyzer.ProvisionRecommendation;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import org.apache.kafka.clients.admin.NewTopic;
+
+import static com.linkedin.cruisecontrol.common.utils.Utils.validateNotNull;
+import static com.linkedin.kafka.cruisecontrol.common.Utils.getTopicNamesMatchedWithPattern;
+import static com.linkedin.kafka.cruisecontrol.detector.AnomalyDetectorUtils.KAFKA_CRUISE_CONTROL_OBJECT_CONFIG;
+import static com.linkedin.kafka.cruisecontrol.detector.ProvisionerState.State.COMPLETED;
+import static com.linkedin.kafka.cruisecontrol.detector.ProvisionerState.State.COMPLETED_WITH_ERROR;
+import static com.linkedin.kafka.cruisecontrol.detector.ProvisionerUtils.increasePartitionCount;
+import static com.linkedin.kafka.cruisecontrol.detector.ProvisionerUtils.partitionRecommendations;
+
+
+/**
+ * A provisioner that honors {@link ProvisionRecommendation provision recommendations} of partitions and ignores recommendations for other resources.
+ */
+public class PartitionProvisioner implements Provisioner {
+  protected KafkaCruiseControl _kafkaCruiseControl;
+
+  @Override
+  public synchronized ProvisionerState rightsize(Map<String, ProvisionRecommendation> recommendationByRecommender, RightsizeOptions options) {
+    validateNotNull(recommendationByRecommender, "Provision recommendations cannot be null.");
+
+    // 1. Retrieve partition recommendations.
+    Map<String, ProvisionRecommendation> partitionRecommendations = partitionRecommendations(recommendationByRecommender);
+    if (partitionRecommendations.isEmpty()) {
+      // No actions will be taken towards rightsizing.
+      return null;
+    }
+
+    // 2. Apply the partition recommendations.
+    Map<String, ProvisionerState> provisionerStateByRecommender = applyPartitionRecommendations(partitionRecommendations);
+
+    // 3. Aggregate and return the result of applying partition recommendations.
+    return aggregateProvisionerStates(provisionerStateByRecommender);
+  }
+
+  @Override
+  public void configure(Map<String, ?> configs) {
+    _kafkaCruiseControl = (KafkaCruiseControl) validateNotNull(configs.get(KAFKA_CRUISE_CONTROL_OBJECT_CONFIG),
+                                                               () -> String.format("Missing %s when creating Partition Provisioner",
+                                                                                   KAFKA_CRUISE_CONTROL_OBJECT_CONFIG));
+  }
+
+  /**
+   * Applies the provision actions for the given partition recommendations.
+   *
+   * @param partitionRecommendations Partition recommendations by recommender for which the provision actions will be applied.
+   * @return {@link ProvisionerState} by recommender, indicating the result of applying the recommended provision actions.
+   */
+  protected Map<String, ProvisionerState> applyPartitionRecommendations(Map<String, ProvisionRecommendation> partitionRecommendations) {
+    Map<String, ProvisionerState> provisionerStateByRecommender = new HashMap<>();
+    for (Map.Entry<String, ProvisionRecommendation> entry : partitionRecommendations.entrySet()) {
+      provisionerStateByRecommender.put(entry.getKey(), provisionPartition(entry.getValue()));
+    }
+    return provisionerStateByRecommender;
+  }
+
+  /**
+   * Use the information from the given recommendation to provision partitions for each topic matching the given topic pattern to ensure
+   * that each topic has the given number of partitions.
+   *
+   * @param recommendation A provision recommendation of partitions.
+   * @return The {@link ProvisionerState} indicating the result of applying the recommended provision actions.
+   */
+  protected ProvisionerState provisionPartition(ProvisionRecommendation recommendation) {
+    Set<String> topicNames = getTopicNamesMatchedWithPattern(recommendation.topicPattern(), () -> _kafkaCruiseControl.kafkaCluster().topics());
+    Set<NewTopic> topicsToAddPartitions = new HashSet<>(topicNames.size());
+    for (String topicName : topicNames) {
+      topicsToAddPartitions.add(new NewTopic(topicName, Optional.of(recommendation.numPartitions()), Optional.empty()));
+    }
+
+    return increasePartitionCount(_kafkaCruiseControl.adminClient(), topicsToAddPartitions);
+  }
+
+  /**
+   * Aggregate the given results of applying partition recommendations provided by the corresponding recommender.
+   *
+   * @param provisionerStateByRecommender Results of applying partition recommendations provided by the corresponding recommender.
+   * @return The aggregated {@link ProvisionerState} of the provisioning actions, or {@code null} if no result is provided to aggregate.
+   */
+  protected static ProvisionerState aggregateProvisionerStates(Map<String, ProvisionerState> provisionerStateByRecommender) {
+    if (provisionerStateByRecommender.isEmpty()) {
+      return null;
+    }
+
+    // Applying a provision recommendation for partitions would end up either (1) COMPLETED or (2) COMPLETED_WITH_ERROR state.
+    // The aggregate state would be (1) COMPLETED_WITH_ERROR if any result indicates an error, (2) COMPLETED otherwise.
+    // The overall summary indicates the aggregate summary by each recommender.
+    boolean hasAnyResultCompletedWithError = false;
+    StringBuilder aggregateSummary = new StringBuilder();
+    for (Map.Entry<String, ProvisionerState> entry : provisionerStateByRecommender.entrySet()) {
+      if (entry.getValue().state() == COMPLETED_WITH_ERROR) {
+        hasAnyResultCompletedWithError = true;
+      }
+      aggregateSummary.append(String.format("[%s] %s ", entry.getKey(), entry.getValue().summary()));
+    }
+
+    return new ProvisionerState(hasAnyResultCompletedWithError ? COMPLETED_WITH_ERROR : COMPLETED, aggregateSummary.toString().trim());
+  }
+}
--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/ProvisionerUtils.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/detector/ProvisionerUtils.java
@@ -4,10 +4,17 @@
 
 package com.linkedin.kafka.cruisecontrol.detector;
 
+import com.linkedin.kafka.cruisecontrol.analyzer.ProvisionRecommendation;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
 
 import static com.linkedin.kafka.cruisecontrol.KafkaCruiseControlUtils.maybeIncreasePartitionCount;
+import static com.linkedin.kafka.cruisecontrol.detector.ProvisionerState.State.COMPLETED;
+import static com.linkedin.kafka.cruisecontrol.detector.ProvisionerState.State.COMPLETED_WITH_ERROR;
+
 
 /**
  * A util class for provisions.
@@ -18,23 +25,45 @@ public final class ProvisionerUtils {
   }
 
   /**
-   * Determine the status of increasing the partition count
+   * Increase the partition count of the given existing topics to the desired partition count (if needed).
    *
    * @param adminClient AdminClient to handle partition count increases.
-   * @param topicToAddPartitions Existing topic to add more partitions if needed
-   * @return The state {@link ProvisionerState.State#COMPLETED} when true, {@link ProvisionerState.State#COMPLETED_WITH_ERROR} when false
+   * @param topicsToAddPartitions Existing topics to add more partitions to (if needed). More partitions are added to a topic if it has fewer
+   *                              than the number of partitions specified in its corresponding {@link NewTopic#numPartitions()}.
+   * @return {@link ProvisionerState.State#COMPLETED} when none of the partition count increase attempts fail,
+   * {@link ProvisionerState.State#COMPLETED_WITH_ERROR} otherwise.
    */
-  public static ProvisionerState partitionIncreaseStatus(AdminClient adminClient, NewTopic topicToAddPartitions) {
-    if (maybeIncreasePartitionCount(adminClient, topicToAddPartitions)) {
-      String summary = String.format("Provisioning the partition count to increase to %d for the topic %s was successfully completed.",
-                                     topicToAddPartitions.numPartitions(),
-                                     topicToAddPartitions.name());
-      return new ProvisionerState(ProvisionerState.State.COMPLETED, summary);
-    } else {
-      String summary = String.format("Provisioning the partition count to increase to %d for the topic %s was rejected.",
-                                     topicToAddPartitions.numPartitions(),
-                                     topicToAddPartitions.name());
-      return new ProvisionerState(ProvisionerState.State.COMPLETED_WITH_ERROR, summary);
+  public static ProvisionerState increasePartitionCount(AdminClient adminClient, Set<NewTopic> topicsToAddPartitions) {
+    Map<String, Integer> numPartitionsBySucceededTopic = new HashMap<>(topicsToAddPartitions.size());
+    Map<String, Integer> numPartitionsByFailedTopic = new HashMap<>(topicsToAddPartitions.size());
+
+    for (NewTopic topicToAddPartitions : topicsToAddPartitions) {
+      boolean success = maybeIncreasePartitionCount(adminClient, topicToAddPartitions);
+      if (success) {
+        numPartitionsBySucceededTopic.put(topicToAddPartitions.name(), topicToAddPartitions.numPartitions());
+      } else {
+        numPartitionsByFailedTopic.put(topicToAddPartitions.name(), topicToAddPartitions.numPartitions());
+      }
     }
+    String summary = String.format("Setting partition count by topic succeeded: %s failed: %s.",
+                                   numPartitionsBySucceededTopic, numPartitionsByFailedTopic);
+    return new ProvisionerState(numPartitionsByFailedTopic.isEmpty() ? COMPLETED : COMPLETED_WITH_ERROR, summary);
+  }
+
+  /**
+   * Create a new subset map of the given recommendation-by-recommender map containing only the entries, where the resource is a partition.
+   * If the given map has no recommendations, where the resource is a partition, then the response would be an empty map.
+   *
+   * @param recommendationByRecommender Provision recommendations provided by corresponding recommenders.
+   * @return A new subset map of the given recommendation-by-recommender map containing only the entries, where the resource is a partition.
+   */
+  public static Map<String, ProvisionRecommendation> partitionRecommendations(Map<String, ProvisionRecommendation> recommendationByRecommender) {
+    Map<String, ProvisionRecommendation> partitionRecommendations = new HashMap<>();
+    for (Map.Entry<String, ProvisionRecommendation> recommendationEntry : recommendationByRecommender.entrySet()) {
+      if (recommendationEntry.getValue().numPartitions() != ProvisionRecommendation.DEFAULT_OPTIONAL_INT) {
+        partitionRecommendations.put(recommendationEntry.getKey(), recommendationEntry.getValue());
+      }
+    }
+    return partitionRecommendations;
   }
 }
