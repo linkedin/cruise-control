558667cf7b2fb5acae1c24e3b876da53c986c33a
Trevor Currie <tcurrie@twilio.com>
2023-05-24 20:02:01 -0700

    Adding back the deprecated function, maybe a bit much.

--- cruise-control-client/cruisecontrolclient/client/Query.py
+++ cruise-control-client/cruisecontrolclient/client/Query.py
@@ -8,6 +8,24 @@ from cruisecontrolclient.client.Endpoint import AbstractEndpoint
 import warnings
 
 
+def generate_url_from_cc_socket_address(cc_socket_address: str, endpoint: AbstractEndpoint) -> str:
+    """
+    Given a cruise-control hostname[:port] and an Endpoint, return the correct URL
+    for this cruise-control operation.
+    Note that this URL _includes_ parameters.
+    :param cc_socket_address: like hostname[:port], ip-address[:port]
+    :param endpoint:
+    :return: URL, the correct URL to perform the Endpoint's operation
+             on the given cruise-control host, _including parameters_.
+    """
+    warnings.warn("This function is deprecated as of 0.2.0. "
+                  "It may be removed entirely in future versions.",
+                  DeprecationWarning,
+                  stacklevel=2)
+    url = f"http://{cc_socket_address}/kafkacruisecontrol/{endpoint.compose_endpoint()}"
+    return url
+
+
 def generate_base_url_from_cc_socket_address(cc_socket_address: str, endpoint: AbstractEndpoint) -> str:
     """
     Given a cruise-control hostname[:port] and an Endpoint, return the correct URL
