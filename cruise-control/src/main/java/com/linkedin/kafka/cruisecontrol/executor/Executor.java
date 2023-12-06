linkedin/cruise-control

commit c3c3a54f1f70f280de8d70aebd44978205ca38cf

Fix endpoint metrics involving task execution (#2069)
This PR resolves #2068.
This fix has markTaskExecutionFinished to update the endpoint timer metrics when the execution is done and also eliminates the race condition between checkActiveUserTasks and startExecution on updating the metrics.

cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/executor/Executor.java
@@ -1295,6 +1295,7 @@ public class Executor {
     private long _lastSlowTaskReportingTimeMs;
     private static final boolean FORCE_PAUSE_SAMPLING = true;
     private final Timer _executionTimerInvolveBrokerRemovalOrDemotion;
+    private final UserTaskManager.UserTaskInfo _userTaskInfo;
 
     ProposalExecutionRunnable(LoadMonitor loadMonitor,
                               Collection<Integer> demotedBrokers,
@@ -1309,6 +1310,7 @@ public class Executor {
         _noOngoingExecutionSemaphore.release();
         _stopSignal.set(NO_STOP_EXECUTION);
         _executionStoppedByUser.set(false);
+        _userTaskInfo = null;
         LOG.error("Failed to initialize proposal execution.");
         throw new IllegalStateException("User task manager cannot be null.");
       }
@@ -1342,14 +1344,28 @@ public class Executor {
       } else {
         _executionTimerInvolveBrokerRemovalOrDemotion = null;
       }
+
+      // If the task is triggered from a user request, mark the task to be in-execution state in user task manager and
+      // retrieve the associated user task information.
+      if (_isTriggeredByUserRequest) {
+        // For userTaskInfo that involves proposal execution, we would like its successfulRequestExecutionTimer be updated
+        // after the proposal execution being completed and be updated in markTaskExecutionFinished instead of in
+        // checkActiveUserTasks. checkActiveUserTasks would update the timer if _userTaskInfo.isUserTaskDone() is true.
+        // _userTaskInfo.isUserTaskDone() is associated with whether the OptimizationResult is computed, and is not associated
+        // with whether the proposal execution is completed or not. Thus, call markTaskExecutionBegan in the constructor to
+        // remove _userTaskInfo from _uuidToActiveUserTaskInfoMap before _userTaskInfo.isUserTaskDone() being true.
+        _userTaskInfo = _userTaskManager.markTaskExecutionBegan(_uuid);
+      } else {
+        _userTaskInfo = null;
+      }
     }
 
     public void run() {
       LOG.info("Starting executing balancing proposals.");
       final long start = System.currentTimeMillis();
       try {
-        UserTaskManager.UserTaskInfo userTaskInfo = initExecution();
-        execute(userTaskInfo);
+        initExecution();
+        execute(_userTaskInfo);
       } catch (Exception e) {
         LOG.error("ProposalExecutionRunnable got exception during run", e);
       } finally {
@@ -1362,19 +1378,14 @@ public class Executor {
       LOG.info("Execution finished.");
     }
 
-    private UserTaskManager.UserTaskInfo initExecution() {
-      UserTaskManager.UserTaskInfo userTaskInfo = null;
-      // If the task is triggered from a user request, mark the task to be in-execution state in user task manager and
-      // retrieve the associated user task information.
+    private void initExecution() {
       if (_isTriggeredByUserRequest) {
-        userTaskInfo = _userTaskManager.markTaskExecutionBegan(_uuid);
+        _userTaskManager.logInExecutionTaskOperation();
       }
       String reason = _reasonSupplier.get();
       _executorState = ExecutorState.executionStarting(_uuid, reason, _recentlyDemotedBrokers, _recentlyRemovedBrokers, _isTriggeredByUserRequest);
       OPERATION_LOG.info("Task [{}] execution starts. The reason of execution is {}.", _uuid, reason);
       _ongoingExecutionIsBeingModified.set(false);
-
-      return userTaskInfo;
     }
 
     /**

cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/servlet/UserTaskManager.java
@@ -405,12 +405,20 @@ public class UserTaskManager implements Closeable {
 
     if (_uuidToActiveUserTaskInfoMap.containsKey(userTaskId)) {
       _inExecutionUserTaskInfo = _uuidToActiveUserTaskInfoMap.remove(userTaskId).setState(TaskState.IN_EXECUTION);
-      // Normally a user task's operation result is logged when the task's state is transferred from ACTIVE to COMPLETED_WITH_ERROR.
-      // If the user task's state is transferred from ACTIVE directly to IN_EXECUTION, need to log the task's operation result here.
-      _inExecutionUserTaskInfo.logOperation();
     }
 
     return _inExecutionUserTaskInfo;
+  }
+
+  /**
+   * Normally a user task's operation result is logged when the task's state is transferred from ACTIVE to COMPLETED or
+   * COMPLETED_WITH_ERROR. If the user task's state is transferred from ACTIVE directly to IN_EXECUTION,
+   * need to log the task's operation result.
+   */
+  public synchronized void logInExecutionTaskOperation() {
+    if (_inExecutionUserTaskInfo != null) {
+      _inExecutionUserTaskInfo.logOperation();
+    }
   }
 
   /**
@@ -420,7 +428,9 @@ public class UserTaskManager implements Closeable {
    * @param completeWithError Whether the task execution finished with error or not.
    */
   public synchronized void markTaskExecutionFinished(String uuid, boolean completeWithError) {
-    LOG.debug("Task execution with uuid {} completed{}.", uuid, completeWithError ? " with error" : "");
+    final long requestCompleteTime = _time.milliseconds() - _inExecutionUserTaskInfo._startMs;
+    _successfulRequestExecutionTimer.get(_inExecutionUserTaskInfo.endPoint()).update(requestCompleteTime, TimeUnit.MILLISECONDS);
+    LOG.info("Task execution with uuid {} completed{}. Total time: {} ms", uuid, completeWithError ? " with error" : "", requestCompleteTime);
     if (!_inExecutionUserTaskInfo.userTaskId().equals(UUID.fromString(uuid))) {
       throw new IllegalStateException(String.format("Task %s is not found in UserTaskManager.", uuid));
     }

cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/executor/ExecutorTest.java
@@ -485,6 +485,7 @@ public class ExecutorTest extends CCKafkaClientsIntegrationTestHarness {
     UserTaskManager.UserTaskInfo mockUserTaskInfo = getMockUserTaskInfo();
     // This tests runs two consecutive executions. First one completes w/o error, but the second one with error.
     UserTaskManager mockUserTaskManager = getMockUserTaskManager(RANDOM_UUID, mockUserTaskInfo, Arrays.asList(false, true));
+    mockUserTaskManager.logInExecutionTaskOperation();
     EasyMock.replay(mockMetadataClient, mockLoadMonitor, mockAnomalyDetectorManager, mockUserTaskInfo, mockUserTaskManager);
 
     Collection<ExecutionProposal> proposalsToExecute = Collections.singletonList(proposal);
@@ -699,6 +700,7 @@ public class ExecutorTest extends CCKafkaClientsIntegrationTestHarness {
                                                         UserTaskManager.UserTaskInfo userTaskInfo,
                                                         List<Boolean> completeWithError) {
     UserTaskManager mockUserTaskManager = EasyMock.mock(UserTaskManager.class);
+    mockUserTaskManager.logInExecutionTaskOperation();
     // Handle the case that the execution started, but did not finish.
     if (completeWithError.isEmpty()) {
       EasyMock.expect(mockUserTaskManager.markTaskExecutionBegan(uuid)).andReturn(userTaskInfo).once();

commit 9ccbb9eeb497b23d9e98e76f7512abea908366af

Fix deadlock between Executor and UserTaskManager (#2086)
This PR fixes the deadlock introduced by PR #2069 by partially reverting changes in that PR by moving _userTaskManager.markTaskExecutionBegan(_uuid) from the constructor of ProposalExecutionRunnable back to its run() thread. This avoids the deadlock formed between Executor and UserTaskManager. The initialization of ProposalExecutionRunnable is under Executor's monitor lock and _userTaskManager.markTaskExecutionBegan aquires UserTaskManager's monitor lock. On the other hand, the process of StopProposalRequest enters UserTaskManager's monitor lock first, and then acquires Executor's monitor lock.

With this change, we would have the issue back that [request]-successful-request-execution-timer may or may not record the proposal computing time for request that requires proposal execution depending on the race condition between the task is removed from _uuidToActiveUserTaskInfoMap and checkActiveUserTasks is called. This issue will be addressed in a separate PR.

cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/executor/Executor.java
@@ -1295,7 +1295,6 @@ public class Executor {
     private long _lastSlowTaskReportingTimeMs;
     private static final boolean FORCE_PAUSE_SAMPLING = true;
     private final Timer _executionTimerInvolveBrokerRemovalOrDemotion;
-    private final UserTaskManager.UserTaskInfo _userTaskInfo;
 
     private final Collection<Integer> _demotedBrokers;
 
@@ -1316,7 +1315,6 @@ public class Executor {
         _noOngoingExecutionSemaphore.release();
         _stopSignal.set(NO_STOP_EXECUTION);
         _executionStoppedByUser.set(false);
-        _userTaskInfo = null;
         LOG.error("Failed to initialize proposal execution.");
         throw new IllegalStateException("User task manager cannot be null.");
       }
@@ -1350,28 +1348,14 @@ public class Executor {
       } else {
         _executionTimerInvolveBrokerRemovalOrDemotion = null;
       }
-
-      // If the task is triggered from a user request, mark the task to be in-execution state in user task manager and
-      // retrieve the associated user task information.
-      if (_isTriggeredByUserRequest) {
-        // For userTaskInfo that involves proposal execution, we would like its successfulRequestExecutionTimer be updated
-        // after the proposal execution being completed and be updated in markTaskExecutionFinished instead of in
-        // checkActiveUserTasks. checkActiveUserTasks would update the timer if _userTaskInfo.isUserTaskDone() is true.
-        // _userTaskInfo.isUserTaskDone() is associated with whether the OptimizationResult is computed, and is not associated
-        // with whether the proposal execution is completed or not. Thus, call markTaskExecutionBegan in the constructor to
-        // remove _userTaskInfo from _uuidToActiveUserTaskInfoMap before _userTaskInfo.isUserTaskDone() being true.
-        _userTaskInfo = _userTaskManager.markTaskExecutionBegan(_uuid);
-      } else {
-        _userTaskInfo = null;
-      }
     }
 
     public void run() {
       LOG.info("Starting executing balancing proposals.");
       final long start = System.currentTimeMillis();
       try {
-        initExecution();
-        execute(_userTaskInfo);
+        UserTaskManager.UserTaskInfo userTaskInfo = initExecution();
+        execute(userTaskInfo);
       } catch (Exception e) {
         LOG.error("ProposalExecutionRunnable got exception during run", e);
       } finally {
@@ -1396,14 +1380,18 @@ public class Executor {
       }
     }
 
-    private void initExecution() {
+    private UserTaskManager.UserTaskInfo initExecution() {
+      UserTaskManager.UserTaskInfo userTaskInfo = null;
+      // If the task is triggered from a user request, mark the task to be in-execution state in user task manager and
+      // retrieve the associated user task information.
       if (_isTriggeredByUserRequest) {
-        _userTaskManager.logInExecutionTaskOperation();
+        userTaskInfo = _userTaskManager.markTaskExecutionBegan(_uuid);
       }
       String reason = _reasonSupplier.get();
       _executorState = ExecutorState.executionStarting(_uuid, reason, _recentlyDemotedBrokers, _recentlyRemovedBrokers, _isTriggeredByUserRequest);
       OPERATION_LOG.info("Task [{}] execution starts. The reason of execution is {}.", _uuid, reason);
       _ongoingExecutionIsBeingModified.set(false);
+      return userTaskInfo;
     }
 
     /**

cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/servlet/UserTaskManager.java
@@ -406,19 +406,11 @@ public class UserTaskManager implements Closeable {
     if (_uuidToActiveUserTaskInfoMap.containsKey(userTaskId)) {
       _inExecutionUserTaskInfo = _uuidToActiveUserTaskInfoMap.remove(userTaskId).setState(TaskState.IN_EXECUTION);
     }
+    // Normally a user task's operation result is logged when the task's state is transferred from ACTIVE to COMPLETED_WITH_ERROR.
+    // If the user task's state is transferred from ACTIVE directly to IN_EXECUTION, need to log the task's operation result here.
+    _inExecutionUserTaskInfo.logOperation();
 
     return _inExecutionUserTaskInfo;
-  }
-
-  /**
-   * Normally a user task's operation result is logged when the task's state is transferred from ACTIVE to COMPLETED or
-   * COMPLETED_WITH_ERROR. If the user task's state is transferred from ACTIVE directly to IN_EXECUTION,
-   * need to log the task's operation result.
-   */
-  public synchronized void logInExecutionTaskOperation() {
-    if (_inExecutionUserTaskInfo != null) {
-      _inExecutionUserTaskInfo.logOperation();
-    }
   }
 
   /**

cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/executor/ExecutorTest.java
@@ -485,7 +485,6 @@ public class ExecutorTest extends CCKafkaClientsIntegrationTestHarness {
     UserTaskManager.UserTaskInfo mockUserTaskInfo = getMockUserTaskInfo();
     // This tests runs two consecutive executions. First one completes w/o error, but the second one with error.
     UserTaskManager mockUserTaskManager = getMockUserTaskManager(RANDOM_UUID, mockUserTaskInfo, Arrays.asList(false, true));
-    mockUserTaskManager.logInExecutionTaskOperation();
     EasyMock.replay(mockMetadataClient, mockLoadMonitor, mockAnomalyDetectorManager, mockUserTaskInfo, mockUserTaskManager);
 
     Collection<ExecutionProposal> proposalsToExecute = Collections.singletonList(proposal);
@@ -700,7 +699,6 @@ public class ExecutorTest extends CCKafkaClientsIntegrationTestHarness {
                                                         UserTaskManager.UserTaskInfo userTaskInfo,
                                                         List<Boolean> completeWithError) {
     UserTaskManager mockUserTaskManager = EasyMock.mock(UserTaskManager.class);
-    mockUserTaskManager.logInExecutionTaskOperation();
     // Handle the case that the execution started, but did not finish.
     if (completeWithError.isEmpty()) {
       EasyMock.expect(mockUserTaskManager.markTaskExecutionBegan(uuid)).andReturn(userTaskInfo).once();