50352be26fe455e0b716671605d359569bed2853
Allen Wang <allenxwang@gmail.com>
2024-10-02 12:52:40 -0700

    Update cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/async/progress/OperationProgress.java
    Co-authored-by: Maryan Hratson <mhratson@linkedin.com>

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/async/progress/OperationProgress.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/async/progress/OperationProgress.java
@@ -103,7 +103,7 @@ public class OperationProgress {
   /**
    * @return the up-to-date total execution time on the progress
    */
-  public synchronized long getCurrentTotalExecutionTime() {
+  public synchronized long getCurrentTotalExecutionTimeMs() {
     if (_startTimes.isEmpty()) {
       return 0;
     } else {
