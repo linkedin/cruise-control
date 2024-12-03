c53448738eb997c0d10fd62932b3e192eee03570
Renyu Jiao <renyujiao@tubi.tv>
2024-12-03 18:11:38 +0800

    update sign request

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/prometheus/PrometheusAdapter.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/prometheus/PrometheusAdapter.java
@@ -4,38 +4,34 @@
 
 package com.linkedin.kafka.cruisecontrol.monitor.sampling.prometheus;
 
+import static com.linkedin.cruisecontrol.common.utils.Utils.*;
+
+import com.google.gson.Gson;
+import com.linkedin.kafka.cruisecontrol.monitor.sampling.prometheus.model.PrometheusQueryResult;
+import com.linkedin.kafka.cruisecontrol.monitor.sampling.prometheus.model.PrometheusResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import com.google.gson.Gson;
-import com.linkedin.kafka.cruisecontrol.monitor.sampling.prometheus.model.PrometheusQueryResult;
-import com.linkedin.kafka.cruisecontrol.monitor.sampling.prometheus.model.PrometheusResponse;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.signer.Aws4Signer;
-import software.amazon.awssdk.auth.signer.params.Aws4SignerParams;
 import software.amazon.awssdk.http.SdkHttpFullRequest;
 import software.amazon.awssdk.http.SdkHttpMethod;
-import software.amazon.awssdk.regions.Region;
-import org.apache.http.client.utils.URLEncodedUtils;
-import java.io.ByteArrayInputStream;
-
-import static com.linkedin.kafka.cruisecontrol.KafkaCruiseControlUtils.SEC_TO_MS;
-import static com.linkedin.cruisecontrol.common.utils.Utils.validateNotNull;
+import software.amazon.awssdk.http.auth.aws.signer.AwsV4HttpSigner;
+import software.amazon.awssdk.http.auth.spi.signer.SignedRequest;
 
 /**
  * This class provides an adapter to make queries to a Prometheus Server to fetch metric values.
@@ -53,8 +49,8 @@ class PrometheusAdapter {
     private final CloseableHttpClient _httpClient;
     protected final URIBuilder _prometheusEndpoint;
     protected final int _samplingIntervalMs;
-    private final Aws4Signer _signer;
-    private final Aws4SignerParams _signerParams;
+    private final String _region;
+    private final Boolean _use_sigv4;
 
     PrometheusAdapter(CloseableHttpClient httpClient,
         URIBuilder prometheusEndpoint,
@@ -63,18 +59,8 @@ class PrometheusAdapter {
         _httpClient = validateNotNull(httpClient, "httpClient cannot be null.");
         _prometheusEndpoint = validateNotNull(prometheusEndpoint, "prometheusEndpoint cannot be null.");
         _samplingIntervalMs = samplingIntervalMs;
-        
-        if (region != null) {
-            _signer = Aws4Signer.create();
-            _signerParams = Aws4SignerParams.builder()
-                .awsCredentials(DefaultCredentialsProvider.create().resolveCredentials())
-                .signingName(SERVICE_NAME)
-                .signingRegion(Region.of(region))
-                .build();
-        } else {
-            _signer = null;
-            _signerParams = null;
-        }
+        _region = region;
+        _use_sigv4 = region != null;
     }
 
     public int samplingIntervalMs() {
@@ -84,25 +70,26 @@ class PrometheusAdapter {
     public List<PrometheusQueryResult> queryMetric(String queryString,
                                                    long startTimeMs,
                                                    long endTimeMs) throws IOException {
-        URI queryUri = URI.create(_prometheusEndpoint.toString() + QUERY_RANGE_API_PATH);
-        HttpPost httpPost = new HttpPost(queryUri);
+
 
         List<NameValuePair> data = new ArrayList<>();
-        data.add(new BasicNameValuePair(QUERY, queryString));
+        data.add(new BasicNameValuePair("query", "up"));
         /* "start" and "end" are expected to be unix timestamp in seconds (number of seconds since the Unix epoch).
          They accept values with a decimal point (up to 64 bits). The samples returned are inclusive of the "end"
          timestamp provided.
          */
-        data.add(new BasicNameValuePair(START, String.valueOf((double) startTimeMs / SEC_TO_MS)));
-        data.add(new BasicNameValuePair(END, String.valueOf((double) endTimeMs / SEC_TO_MS)));
+        data.add(new BasicNameValuePair("start", "2024-12-02T10:10:30.781Z"));
+        data.add(new BasicNameValuePair("end", "2024-12-02T20:11:00.781Z"));
         // step is expected to be in seconds, and accept values with a decimal point (up to 64 bits).
-        data.add(new BasicNameValuePair(STEP, String.valueOf((double) _samplingIntervalMs / SEC_TO_MS)));
+        data.add(new BasicNameValuePair("step", "15s"));
 
-        httpPost.setEntity(new UrlEncodedFormEntity(data));
-        
+        String queryParams = URLEncodedUtils.format(data, StandardCharsets.UTF_8);
+        URI queryUri = URI.create(_prometheusEndpoint.toString() + QUERY_RANGE_API_PATH + "?" + queryParams);
+        HttpPost httpPost = new HttpPost(queryUri);
+
         // Sign the request if SigV4 is enabled
-        if (_signer != null) {
-            signRequest(httpPost, data);
+        if (_use_sigv4) {
+            signRequest(httpPost, queryUri);
         }
 
         try (CloseableHttpResponse response = _httpClient.execute(httpPost)) {
@@ -135,32 +122,27 @@ class PrometheusAdapter {
         }
     }
     
-    private void signRequest(HttpPost httpPost, List<NameValuePair> data) throws IOException {
-        // Convert form parameters to query string
-        String queryString = URLEncodedUtils.format(data, StandardCharsets.UTF_8);
-        
-        // Convert HttpPost to SdkHttpFullRequest
-        SdkHttpFullRequest.Builder requestBuilder = SdkHttpFullRequest.builder()
-            .method(SdkHttpMethod.POST)
-            .uri(httpPost.getURI())
-            .encodedPath(httpPost.getURI().getPath())
-            .putHeader("Host", httpPost.getURI().getHost())
-            .putHeader("Content-Type", "application/x-www-form-urlencoded");
+    private void signRequest(HttpPost httpPost, URI queryUri) throws IOException {
+        // Get AWS credentials
+        DefaultCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+        AwsCredentials credentials = credentialsProvider.resolveCredentials();
 
-        // Add query string as raw query parameters
-        requestBuilder.rawQueryParameters(
-            Collections.singletonMap("", Collections.singletonList(queryString))
-        );
-        
-        // Add the same content as the request body
-        requestBuilder.contentStreamProvider(() -> 
-            new ByteArrayInputStream(queryString.getBytes(StandardCharsets.UTF_8)));
-        
-        // Sign the request
-        SdkHttpFullRequest signedRequest = _signer.sign(requestBuilder.build(), _signerParams);
-        
-        // Copy signed headers back to HttpPost
-        signedRequest.headers().forEach((key, values) -> 
+        // Create the HTTP request
+        SdkHttpFullRequest.Builder httpRequest = SdkHttpFullRequest.builder()
+            .uri(queryUri)
+            .method(SdkHttpMethod.POST)
+            .putHeader("Content-Type", "application/text");
+
+        AwsV4HttpSigner signer = AwsV4HttpSigner.create();
+
+        SignedRequest signedRequest =
+            signer.sign(r -> r.identity(credentials)
+                .request(httpRequest.build())
+                .putProperty(AwsV4HttpSigner.SERVICE_SIGNING_NAME, SERVICE_NAME)
+                .putProperty(AwsV4HttpSigner.REGION_NAME, _region));
+
+        signedRequest.request().headers().forEach((key, values) ->
             httpPost.setHeader(key, values.get(0)));
+
     }
 }
